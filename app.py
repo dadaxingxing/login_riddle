467a276ba24b6dcebb8454cede5599d971e3ef95
Joey <xiaxing.zhu16@gmail.com>
2024-09-01 19:45:53 -0700

    setup some basic routes and added template html

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-import pymongo
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_bcrypt import Bcrypt
+from pymongo import MongoClient
 
-
-client = pymongo.MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://localhost:27017/')
 db = client["mydatabase"]
 user_info = db['users']
 
-
-test_data = {
-    'username': 'amongus',
-    'password': 'imposter123'
-}
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+bcrypt = Bcrypt(app)
 
 
-user_info.insert_one(test_data)
-found = user_info.find_one({'username': 'amongus'}) 
-if found:
-    print('yes')
+@app.route('/')
+def default():
+    return 'hello!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = user_info.find_one({'username': username})
+
+        if user and bcrypt.check_password_hash(user['password'], password):
+            session['user'] = username
+            return redirect(url_for('login'))
+        else:
+            return 'Invalid Credential'
+
+    return render_template('login.html')
