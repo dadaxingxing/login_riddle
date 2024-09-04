394bc0f0681311c0148fc8f57f3ac1b960d73403
Joey <xiaxing.zhu16@gmail.com>
2024-09-04 13:32:46 -0700

    added dashboard route

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ bcrypt = Bcrypt(app)
 
 @app.route('/')
 def default():
-    return 'hello!'
+    return redirect(url_for('/login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -24,8 +24,45 @@ def login():
 
         if user and bcrypt.check_password_hash(user['password'], password):
             session['user'] = username
-            return redirect(url_for('login'))
+            return redirect(url_for('dashboard'))
         else:
             return 'Invalid Credential'
 
     return render_template('login.html')
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing = user_info.find_one({'username': username})
+
+        if existing is None:
+            # Hash the password and store it in the database
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            user_info.insert_one({'username': username, 'password': hashed_password})
+            
+            session['user'] = username
+            return redirect(url_for('login'))
+        else:
+            return 'Existing already exist!'
+    
+    return render_template('register.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    
+    if 'user' in session:
+        username = session['user']
+        return render_template('dashboard.html', username=username)
+    else:
+        return redirect(url_for('login'))
+    
+
+@app.route('/logout')
+def logout():
+    del session['user']
+    return redirect(url_for('login'))
